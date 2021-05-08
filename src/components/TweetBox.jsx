a0a86675bd68eee1af1051e31415f01f774291f7
divesh keswaniu <diveshkswn@gmail.com>
2021-05-08 23:57:16 +0530

    Tweetbox component completed

--- src/components/TweetBox.jsx
+++ src/components/TweetBox.jsx
@@ -9,8 +9,10 @@ function TweetBox() {
       <form action="">
         <div className="tweetBox__input">
           <Avatar src="https://cdn2.vectorstock.com/i/1000x1000/20/76/man-avatar-profile-vector-21372076.jpg" />
+          <input type="text" placeholder="What's happening ? " />
         </div>
-        <Button>Tweet</Button>
+        <input type="text" className="tweetBox__image" placeholder="ENter image url here" />
+        <Button className="tweetbox__Button">Tweet</Button>
       </form>
 
     </div>
