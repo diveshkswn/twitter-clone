bb964ad149b434eea518282046c1c3d70d0999c5
divesh keswaniu <diveshkswn@gmail.com>
2021-05-10 23:11:24 +0530

    widgets component done

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -1,10 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
+import { SearchOutlined } from '@material-ui/icons';
+import { TwitterTweetEmbed, TwitterTimelineEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 import React from 'react';
 import './Widgets.css';
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchOutlined className="widgets__SeachIcon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+
+      <div className="widgets__widgetsContainer">
+        <h2>What's happening</h2>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="reactjs"
+          options={{ height: 300 }}
+        />
+        <TwitterTweetEmbed tweetId="1390387635961610242" />
+        <TwitterTweetEmbed tweetId="1390728144676601857" />
+        <TwitterOnAirButton
+          id="1390728144676601857"
+        />
+
+      </div>
     </div>
 
   );
